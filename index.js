dde78c89460f65dfee32e9870fe9f2fafc3edd25
Bertrand256 <blogin@protonmail.com>
2022-12-21 10:52:36 +0100

    Some tests ... 4

--- index.js
+++ index.js
@@ -3,24 +3,31 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    const { owner: owner, repo: repo } = github.context.repo;
+    const context = github.context;
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
+    const { owner, repo } = context.repo;
+
     const tag = core.getInput('tag', { required: true })
         .replace('refs/tags/', '');
-    const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
 
-    const getReleaseResponse = await octokit.request('GET /repos/{owner}/{repo}/releases/tags/{tag}',{
+    const getReleaseResponse = await octokit.rest.repos.getReleaseByTag({
       owner,
       repo,
       tag
     });
 
     const {
-      data: { upload_url: uploadUrl }
+      data: { id: releaseId, html_url: htmlUrl, upload_url: uploadUrl, name: name, body: body, draft: draft, prerelease: prerelease, author: author }
     } = getReleaseResponse;
 
-    // core.setOutput('upload_url', uploadUrl);
-    core.info(`upload_url: ${uploadUrl}`);
+    console.log(`Got release info: '${releaseId}', '${htmlUrl}', '${uploadUrl}', '${name}', '${draft}', '${prerelease}', '${body}', '${author}'`);
+
+    // Set the output variables for use by other actions: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+    core.setOutput("upload_url", uploadUrl);
+
   } catch (error) {
+    console.log(error);
+    core.setFailed(error.message);
     core.info(`err: ${error.message}`);
   }
 }
